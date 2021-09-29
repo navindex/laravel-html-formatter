7567adad7f4e4d4f44b37af949aeec3fd6de28e6
Navindex <mk@navindex.com.au>
2021-09-29 11:54:10 +0800

    Namespace fix

---
+++ tests/HtmlFormatterTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Navindex\LaravelHtmlFormatter\Tests;
+
+use Navindex\LaravelHtmlFormatter\HtmlFormatter;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Navindex\SimpleConfig\Config
+ */
+final class ConfigTest extends TestCase
+{
+}
